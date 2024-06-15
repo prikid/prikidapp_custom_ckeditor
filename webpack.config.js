b077f942013f146335f27dc3802b575d8dcf6750
Serhii Riabcheniuk <serhii.riabcheniuk@gmail.com>
2024-06-15 12:57:49 +0300

    Updated packages and configured settings

--- webpack.config.js
+++ webpack.config.js
@@ -7,23 +7,32 @@
 
 /* eslint-env node */
 
-const path = require( 'path' );
-const webpack = require( 'webpack' );
-const { bundler, styles } = require( '@ckeditor/ckeditor5-dev-utils' );
-const CKEditorWebpackPlugin = require( '@ckeditor/ckeditor5-dev-webpack-plugin' );
-const TerserPlugin = require( 'terser-webpack-plugin' );
+const path = require('path');
+const webpack = require('webpack');
+const CKEStyles = require('@ckeditor/ckeditor5-dev-utils').styles
+const CKEditorWebpackPlugin = require('@ckeditor/ckeditor5-dev-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
+const CKERegex = {
+	svg: /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/,
+	css: /ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css/,
+};
+const targetSVG = /(\.(png|jpe?g|gif|webp|avif)$|^((?!font).)*\.svg$)/;
+const targetFont = /(\.(woff2?|ttf|eot|otf)$|font.*\.svg$)/;
+const targetCSS = /\.p?css$/;
+
+
+const config = {
 	devtool: 'source-map',
-	performance: { hints: false },
+	performance: {hints: false},
 
-	entry: path.resolve( __dirname, 'src', 'ckeditor.js' ),
+	entry: path.resolve(__dirname, 'src', 'ckeditor.js'),
 
 	output: {
 		// The name under which the editor will be exported.
 		library: 'ClassicEditor',
 
-		path: path.resolve( __dirname, 'build' ),
+		path: path.resolve(__dirname, 'build'),
 		filename: 'ckeditor.js',
 		libraryTarget: 'umd',
 		libraryExport: 'default'
@@ -31,8 +40,7 @@ module.exports = {
 
 	optimization: {
 		minimizer: [
-			new TerserPlugin( {
-				sourceMap: true,
+			new TerserPlugin({
 				terserOptions: {
 					output: {
 						// Preserve CKEditor 5 license comments.
@@ -40,31 +48,33 @@ module.exports = {
 					}
 				},
 				extractComments: false
-			} )
+			})
 		]
 	},
 
 	plugins: [
-		new CKEditorWebpackPlugin( {
+		new CKEditorWebpackPlugin({
 			// UI language. Language codes follow the https://en.wikipedia.org/wiki/ISO_639-1 format.
 			// When changing the built-in language, remember to also change it in the editor's configuration (src/ckeditor.js).
 			language: 'en',
-			additionalLanguages: 'all'
-		} ),
-		new webpack.BannerPlugin( {
-			banner: bundler.getLicenseBanner(),
-			raw: true
-		} )
+			// additionalLanguages: 'all'
+			addMainLanguageTranslationsToAllAssets: true
+		})
+
+		// new webpack.BannerPlugin( {
+		// 	banner: bundler.getLicenseBanner(),
+		// 	raw: true
+		// } )
 	],
 
 	module: {
 		rules: [
 			{
-				test: /\.svg$/,
-				use: [ 'raw-loader' ]
+				test: CKERegex.svg,
+				use: ['raw-loader']
 			},
 			{
-				test: /\.css$/,
+				test: CKERegex.css,
 				use: [
 					{
 						loader: 'style-loader',
@@ -75,17 +85,43 @@ module.exports = {
 							}
 						}
 					},
+					'css-loader',
 					{
 						loader: 'postcss-loader',
-						options: styles.getPostCssConfig( {
-							themeImporter: {
-								themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' )
-							},
-							minify: true
-						} )
+						options: {
+							postcssOptions: CKEStyles.getPostCssConfig({
+								themeImporter: {
+									themePath: require.resolve('@ckeditor/ckeditor5-theme-lark')
+								},
+								minify: true
+							})
+						}
 					}
 				]
 			}
 		]
-	}
+	},
+	// resolve: {
+	// 	fallback: {
+	// 		"fs": false,
+	// 		"path": false,
+	// 		"os": false,
+	// 	},
+	// }
 };
+
+config.module.rules.forEach((rule) => {
+	if (rule.test.toString() === targetSVG.toString()) {
+		rule.exclude = CKERegex.svg;
+	}
+
+	if (rule.test.toString() === targetFont.toString()) {
+		rule.exclude = CKERegex.svg;
+	}
+
+	if (rule.test.toString() === targetCSS.toString()) {
+		rule.exclude = CKERegex.css;
+	}
+});
+
+module.exports = config;
